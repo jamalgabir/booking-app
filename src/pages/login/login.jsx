2a46d3e0dfe811e7b1903ec4158102d8cbe6c185
jamalgabir <jamalgamy@gmail.com>
2023-04-23 23:29:11 +0100

    make it better

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 import  { useContext, useState } from 'react';
-import { Navigate } from 'react-router';
 import IsLoading from '../../components/spinner/isloading';
 import { AuthContext } from '../../context/auothContext';
 import "./login.css";
+import { Link } from 'react-router-dom';
 const Login = () => {
     
-    
+    const [msg,setMsg] = useState('');
     const [credentials, setCrededtials] = useState({
         email:undefined,
         password:undefined,
@@ -21,6 +21,7 @@ const Login = () => {
    
    const handleClick =async (e)=>{
     e.preventDefault()
+    
     dispatch({type:"LOGIN_START"});
     try{
         const res = await axios.post("//localhost:5000/auth/login",credentials)
@@ -29,22 +30,25 @@ const Login = () => {
         
 
     }catch(error){
-        
+        setMsg(error.response.data.message)
         dispatch({type:"LOGIN_FAILURE",payload:error.response.data})
     }
     
    }
    
   return (
+    <div className='lcontainer'>
     <div className='login'>
         <h2 style={{textAlign:"center"}}>Login</h2>
-        <div className="lcontainer">
+        <div className="infocontainer">
             <input className='linput'name='email' type="text" onChange={handleChange} placeholder='Email'/>
             <input className='linput' name='password' onChange={handleChange} placeholder='Password' type={"password"}/>
-            
-            {others.loading?<div><IsLoading/></div>:<button type='submit' onClick={handleClick}>Login</button>}
+            {msg&&<p style={{color:"red"}}>{msg}</p>}
+            {others.loading?<div><IsLoading/></div>:<button className='btn' type='submit' onClick={handleClick}>Login</button>}
+            <Link to={"/register"}><p style={{marginTop:"30px",color:"white"}}>Rigister</p></Link>
         </div>
         
+    </div>
     </div>
   )
 }
