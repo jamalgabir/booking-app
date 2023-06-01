2bb4f2cf3efbc08b8e9cb00fa7856858886fd2e5
Jamal Ismail <77586693+jamalgabir@users.noreply.github.com>
2023-06-01 14:53:29 +0100

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,12 +5,16 @@ const Navbar = () => {
     <div className="navbar">
       <div className="navContainer">
         <Link style={{textDecoration:"none",color:"inherit"}} to="/">
-        <span className="logo">lamabooking</span>
+        <span className="logo">J.booking</span>
         </Link>
         
         <div className="navItems">
+          <Link style={{textDecoration:"none",color:"inherit"}} to="/register">
           <button className="navButton">Register</button>
+          </Link>
+          <Link style={{textDecoration:"none",color:"inherit"}} to="/login">
           <button className="navButton">Login</button>
+          </Link>
         </div>
       </div>
     </div>
